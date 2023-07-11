11371812ab731f74a342e5d17d9ab9d714a81f66
zhuhengtan <zhuhengtan@126.com>
2023-07-11 10:45:52 +0800

    fix: type

--- src/type.ts
+++ src/type.ts
@@ -9,12 +9,19 @@ export interface JsonObject {
   [key: string]: null | number | string | Array<JsonObject> | JsonObject
 }
 
+export enum PageType {
+  HandWrited,
+  Generated,
+}
+
 export type CustomRouteObject = Omit<RouteObject, 'children'> & {
   hidden?: boolean
   name?: string
   icon?: React.ReactNode
   checkAuth?: boolean
+  pageType?: PageType
   children?: CustomRouteObject[]
+  entityName?: string
 }
 
 export interface AdminInfo {
